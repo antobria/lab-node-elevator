1ea870a39111b58a465c204ac5c96b7da6277197
Antonella Briatore <antobria@gmail.com>
2018-01-03 15:01:49 +0100

    first iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,30 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.direction = [];
+    this.MAXFLOOR = 10;
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    setInterval(this.update(), 1000);
+  }
+  stop() {
+    clearInterval(this.start());
+  }
+
+  update() {
+    this.log();
+  }
+
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {}
+  floorDown() {}
+  call() {}
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
